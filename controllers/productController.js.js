f1f16b22daa38b85ae6d756919e7eb1fdae3eded
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-24 20:15:33 +0530

    crud

--- controllers/productController.js.js
+++ controllers/productController.js.js
@@ -23,3 +23,52 @@ exports.getProductsByCategory = async (req, res) => {
 };
 
 
+// Create a new product
+exports.createProduct = async (req, res) => {
+  const { name, category, price, image, description, quantity } = req.body;
+  try {
+    const newProduct = new Product({ name, category, price, image, description, quantity });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, category, price, image, description, quantity } = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { name, category, price, image, description, quantity },
+      { new: true } // This returns the updated product
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const { getProducts, getProductsByCategory } = require('../controllers/productController.js');
+const { getProducts, getProductsByCategory, createProduct,  updateProduct, deleteProduct } = require('../controllers/productController.js');
 const router = express.Router();
 
-router.get('/', getProducts);
+router.put('/:id', updateProduct); //update
+router.post('/', createProduct); //create
+
+router.get('/', getProducts); //get all product
 router.get('/category/:category', getProductsByCategory);
 // When a client sends a GET request to this URL the getProductsByCategory function is invoked. It retrieves the category from the URL and fetches all products that belong to that category and same as getproducts
+
+router.delete('/:id', deleteProduct); //delete
 module.exports = router;
